27e3c1fb74896869b5d4792dc2169248a4f6b5cb
Paris Dolfman <parisdolfman@gmail.com>
2017-11-12 20:43:51 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,12 @@
 def fizzbuzz(int)
 if int % 3 == 0 && int % 5 == 0
-  puts "Fizzbuzz"
+  puts "FizzBuzz"
 elsif int % 3 == 0
   puts "Fizz"
 elsif int % 5 == 0
   puts "Buzz"
-else puts int
+else 
+  puts int
 
  end
 end
